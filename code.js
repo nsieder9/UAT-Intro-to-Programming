8c7bf4efcc8f9048fb1f5313076a56c59bcdfb6e
Nate <113061023+nsieder9@users.noreply.github.com>
2022-10-19 13:22:11 -0700

    Add files via upload

--- code.js
+++ code.js
@@ -23,5 +23,47 @@ function runtimer (pageelement){
     
 
 }
-function startbtnclick(){}
-function stopbtnclick(){}
+function getuserinput(){
+fullname =""
+
+do{
+firstname = prompt("first name: ");
+lastname = prompt("last name: ");
+
+fullname = firstname + " " + lastname;
+
+if (fullname.length > 20){
+    alert("Please enter shorter names (less than 20 characters). Length entered: " + fullname.length)
+}
+
+}while(fullname.length > 20);
+
+
+
+
+
+badgenumber = 0
+
+do{
+    badgenumber = prompt("Please enter your badge number: ")
+
+    if (badgenumber > 1000){
+        alert("Are you sure that is your badge number?")
+    }
+
+}while(badgenumber > 1000);
+
+alert("This is your fullname: " + fullname + " and your badgenumber is: " + badgenumber);
+
+
+}
+//These functions are for a later assignment
+function startbtnclick(){
+document.getElementById("BtnStart").disabled=true;
+document.getElementById("BtnStop").disabled=false;
+}
+function stopbtnclick(){
+    document.getElementById("BtnStart").disabled=false;
+    document.getElementById("BtnStop").disabled=true;
+}
+//this is a work in progress for a later assignment
